38838023d56f6a72291807c2d72b30c636656434
Jo Kroese <jo@jokroese.com>
2023-08-04 20:44:01 +0100

    be explicit in functions

--- R/fit.R
+++ R/fit.R
@@ -1,15 +1,21 @@
 #' Fit
 #'
+#' @description
+#' Create a joint distribution from a list of target distributions
+#'
+#'
 #' @param targets
 #' @param empty_joint
 #' @param iterations
-#' @param remove_zeroes Often cells with zero people in are not required in an analysis.
+#' @param remove_zeroes If cells with zero people are not required for your analysis,
+#' removing zeroes will speed up the fit
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fit <- function(targets, empty_joint = NULL, iterations = 10, remove_zeroes = TRUE) {
+fit <- function(targets, empty_joint = NULL, iterations = 10,
+                remove_zeroes = TRUE) {
 
   check_for_duplicate_strata(targets)
 
@@ -22,14 +28,14 @@ fit <- function(targets, empty_joint = NULL, iterations = 10, remove_zeroes = TR
 
   # set up initial frame
   joint <- empty_joint |>
-    mutate(n = 1)
+    dplyr::mutate(n = 1)
 
   total_iterations <- iterations * length(targets)
 
   for (i in 1:total_iterations) {
     target_marginal <- targets[[(i%%length(targets))+1]]
     joint <- single_fit_iteration(joint, target_marginal)
-    if(remove_zeroes) { joint <- joint |> filter(n > 0) }
+    if(remove_zeroes) { joint <- joint |> dplyr::filter(n > 0) }
   }
 
   return(joint)
@@ -39,8 +45,8 @@ check_for_duplicate_strata <- function(targets) {
   for (i in seq_along(targets)) {
 
     # Remove the 'n' column before checking for duplicates
-    tibble_without_n <- targets[[i]] %>%
-      select(-n)
+    tibble_without_n <- targets[[i]] |>
+      dplyr::select(-n)
 
     if (any(duplicated(tibble_without_n))) {
       stop(glue::glue("Error: There are repeated stratas in tibble {i}."))
@@ -58,11 +64,6 @@ single_fit_iteration <- function(joint, target_marginal) {
   marginal <- create_marginal(joint, target_marginal)
   difference <- check_difference(marginal)
   print(difference)
-  # if(difference < threshold) {
-  #   message(glue::glue("Iterations ended due to the difference ({difference})
-  #                      falling below the threshold ({threshold})."))
-  #   break
-  # }
   joint <- update_joint_to_marginal(joint, marginal)
 
   return(joint)
@@ -70,36 +71,36 @@ single_fit_iteration <- function(joint, target_marginal) {
 
 create_marginal <- function(joint, target_marginal) {
   variables_to_align <- target_marginal |>
-    select(-n) |>
+    dplyr::select(-n) |>
     names()
   current_marginal <- joint |>
     dplyr::group_by(!!!rlang::syms(variables_to_align)) |>
-    summarise(current_n = sum(n)) |>
-    ungroup()
+    dplyr::summarise(current_n = sum(n)) |>
+    dplyr::ungroup()
   marginal <- current_marginal |>
-    left_join(target_marginal |>
-                rename(target_n = n),
-              by = join_by(!!!rlang::syms(variables_to_align)))
+    dplyr::left_join(target_marginal |>
+                       dplyr::rename(target_n = n),
+                     by = dplyr::join_by(!!!rlang::syms(variables_to_align)))
 }
 
 update_joint_to_marginal <- function(joint, marginal) {
   marginal <- marginal |>
-    mutate(ratio = case_when(
+    dplyr::mutate(ratio = dplyr::case_when(
       target_n == 0 ~ 0,
       is.na(target_n) ~ 1,
       TRUE ~ target_n/current_n))
   joint <- joint |>
-    left_join(marginal) |>
-    mutate(n = n*ratio) |>
-    select(-current_n, -target_n, -ratio)
+    dplyr::left_join(marginal) |>
+    dplyr::mutate(n = n*ratio) |>
+    dplyr::select(-current_n, -target_n, -ratio)
   return(joint)
 }
 
 check_difference <- function(marginal) {
   difference <-  marginal |>
-    summarise((target_n - current_n) |>
-                abs() |>
-                mean(na.rm = TRUE)) |>
+    dplyr::summarise((target_n - current_n) |>
+                       abs() |>
+                       mean(na.rm = TRUE)) |>
     as.numeric()
   return(difference)
 }
