a5a3c5cf6e61b7139c47d68aa55dceaf37b6a261
Jo Kroese <jo@jokroese.com>
2023-08-06 21:28:46 +0100

    add data.table backend for create_empty_joint

--- tests/testthat/test-create_empty_joint.R
+++ tests/testthat/test-create_empty_joint.R
@@ -34,3 +34,24 @@ test_that("create_empty_joint excludes columns", {
   expect_true("b" %in% names(result))
   expect_true("d" %in% names(result))
 })
+
+test_that("create_empty_joint datatable backend gives the same result as dplyr
+backend (ignoring permutations and attributes differences)", {
+  targets <- list(tibble::tibble(a = 1:5, b = 5:9),
+                  tibble::tibble(b = 5:9),
+                  tibble::tibble(c = 2:6, d = 3:7))
+  columns_to_exclude <- c("a", "c")
+
+  result_dplyr <- create_empty_joint(targets, columns_to_exclude,
+                                     backend = "dplyr")
+  result_datatable <- create_empty_joint(targets, columns_to_exclude,
+                                         backend = "datatable")
+
+  result_dplyr <- result_dplyr |>
+    arrange(b, d)
+
+  result_dplyr <- data.frame(result_dplyr)
+  result_datatable <- data.frame(result_datatable)
+
+  expect_equal(result_dplyr, result_datatable)
+})
